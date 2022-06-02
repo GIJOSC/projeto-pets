1bc582c4077cb5130d87b31198545054e63ef453
Gesiel Neves <neves.gesiel@gmail.com>
2022-06-02 02:56:05 +0044

    Created list

---
+++ src/ui/components/Lista/Lista.style.tsx
@@ -0,0 +1,13 @@
+import { styled } from '@mui/material'
+
+export  const ListaStyled = styled('ul') ``;
+
+export const ItemLista = styled('li') ``;
+
+export const Foto = styled('img') ``;
+
+export const Informacoes = styled('div') ``;
+
+export const Nome = styled('h2') ``;
+
+export const Descricao = styled('p') ``;
